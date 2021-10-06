c2320a2a979ac7bd0bd81e59be5bc3dd9fb9c289
saikiran reddy <saikiran199@gmail.com>
2021-10-06 17:35:45 +0530

    added git notes

---
+++ new.py
@@ -0,0 +1,6 @@
+# new 
+
+print("hello world")
+
+# git status --> to check the status of the files --> red --> untracked files, modified files
+                                            #    --> green added to git
