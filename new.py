2e1d6257e444f36f65b95a5eec818525bbe2b75a
saikiran reddy <saikiran199@gmail.com>
2021-10-06 17:39:11 +0530

    modified file

--- new.py
+++ new.py
@@ -4,3 +4,7 @@ print("hello world")
 
 # git status --> to check the status of the files --> red --> untracked files, modified files
                                             #    --> green added to git
+
+# git add new.py --> to add a file to git
+# git commit -m 'initial commit'
+# git push origin main
