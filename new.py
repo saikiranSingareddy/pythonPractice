2106aed3193fc0d3f6cc3ae4e4f592c4b202b4fb
saikiran reddy <saikiran199@gmail.com>
2021-10-15 16:28:17 +0530

    modified new file

--- new.py
+++ new.py
@@ -5,9 +5,24 @@ print("hello world")
 # git status --> to check the status of the files --> red --> untracked files, modified files
                                             #    --> green added to git
 
-# git add new.py --> to add a file to git
-# git commit -m 'initial commit'
-# git push origin main
+# git add new.py --> to add a file to git(new.py is a file name)
+# git commit -m 'initial commit' == to know that why the version created
+# git push origin main == used to push the file to the github
 # git log --> to check all the commits
 # git checkout <commit-hash>   --> go to a particular commit or version
 # git checkout main --> to checkout to the latest version
+
+#cmd virtual env
+#cd == change directory
+#cd env == opening env file
+#dir == to see the directory
+# mkdir project == to create a new file named projects
+# pip install vitrualenvwrapper ==only need first time to install virtual env
+# python -m venv env
+# cd env
+# dir
+# cd Scripts
+# dir
+# actiavte.bat(file name so no need of cd) === by doing all the above virtual env is created
+#env\Scripts\activate.bat == above procedure can be done in single by using this command
+
